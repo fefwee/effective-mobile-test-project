85bb52a5d1e7e8c6c9a68fdea713c6b9ac783313
fefwee <aweo.wew@mail.ru>
2023-08-19 22:05:48 +0300

    change version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthPagesComponent } from './pages/auth-pages/auth-pages.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthPagesComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
